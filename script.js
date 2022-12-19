762b08fa6614447dbede3c0e997b29a3450b9089
bszeidl <szeidl.balazs@hotmail.com>
2022-12-19 22:04:45 +0100

    Rainbow rectangles are ready.

--- script.js
+++ script.js
@@ -1,7 +1,28 @@
 function pageLoaded () {
     var canvas = document.getElementById('myCanvas');
     var context = canvas.getContext('2d');
-    context.fillRect(175,100,100,100);  
+    
+    let h = 360;
+    let s = 60;
+    let l = 45;
+
+    let position = 0;
+    let width = 450;
+    let height = 350;
+
+     
+
+    for (i = 0; i < 17; i++) {
+
+        context.fillStyle = `hsl(${h},${s}%,${l}%)`
+
+        context.fillRect(position,position,width,height);
+
+        position += 10;
+        width -=20;
+        height -= 20;
+        h -= 360 / 17
+    }
 
 }
 
